c981563619b3010dc20c840727db7e561011e267
yankay <yankai@ubuntu.(none)>
2012-07-15 17:36:17 +0800

    update

--- src/util/Logger.h
+++ src/util/Logger.h
@@ -11,12 +11,40 @@
 #include <string>
 #include <iostream>
 #include <ostream>
+#include <sstream>
 
 using namespace std;
 
+namespace libh {
+
+class Logger {
+
+
+public:
+	enum loglevel {
+		WARN, DEBUG, ERROR
+	};
+
+
+	Logger(const string& name, loglevel level) :
+			name(name), level(level) {
+	}
+
+	Logger warn(loglevel level) {
+		this->level = level;
+		return *this;
+	}
+
+
+private:
+	string name;
+	loglevel level;
+};
+
+}  // namespace libhadoop
 
 #define LOG_OS cout
-#define LOG_WARN(name, expression) LOG_OS << "WARNING [" << name << "]: " << expression << endl
+#define LOG_WARN(name, expression) LOG_OS << "WARN [" << name << "]: " << expression << endl
 #define LOG_ERROR(name, expression) LOG_OS << "ERROR [" << name << "]: " << expression << endl
 #define LOG_DEBUG(name, expression) LOG_OS << "DEBUG [" << name << "]: " << expression << endl
 
